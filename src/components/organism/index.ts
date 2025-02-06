efd58be39e933ced7b86244abb48176c9c333f86
pablomesquita <pablomesquita06@gmail.com>
2025-02-06 19:55:00 -0300

    refactoring

--- src/components/organism/index.ts
+++ src/components/organism/index.ts
@@ -4,6 +4,7 @@ import { Header } from "./Header";
 import { TabBar } from "./TabBar";
 import { Layout } from "./Layout";
 import { Comment } from "./Comment";
+import { Messages } from "./Messages";
 import { CardPost } from "./CardPost";
 import { FindCity } from "./FindCity";
 import { ModalGeneric } from "./Modal";
@@ -13,6 +14,7 @@ import { GenericCommet } from "./GenericCommet";
 import { ProfilePicture } from "./ProfilePicture";
 import { LayoutAbstract } from "./LayoutAbstract";
 import { HalfScreenModal } from "./HalfScreenModal";
+
 export {
   Cards,
   Header,
@@ -22,6 +24,7 @@ export {
   Comment,
   CardPost,
   FindCity,
+  Messages,
   HeaderPages,
   ModalGeneric,
   YourLocation,
--- src/screens/Hearts/index.tsx
+++ src/screens/Hearts/index.tsx
@@ -1,97 +1,10 @@
-import { Image, TouchableOpacity } from "react-native";
-import React, { useState } from "react";
-
-import * as S from "./styles";
-import { mocks } from "@/services/mocks";
-import { Layout } from "@/components/organism";
+import React from "react";
+import {  Messages } from "@/components/organism";
 
 export const Hearts = () => {
-  const [tabActive, setTabActive] = useState("Main");
-  const [text, setText] = useState("");
-
-  const messages = [
-    {
-      id: 1,
-      active: true, 
-      name: "Ryan Brooks",
-      photo: "https://exemplo.com/ryan.jpg",
-      messages: ["Tell me we weren’t just talking about this??"], 
-      unread: 1, 
-    },
-    {
-      id: 2,
-      active: false,
-      name: "Logan Harris",
-      photo: "https://exemplo.com/logan.jpg",
-      messages: ["Sent you a post"], 
-      unread: 0, 
-    },
-  ];
-
   return (
-    <Layout>
-      <S.ContainerTabs>
-        <S.Tabs>
-          {mocks.tabs.Main.map((tab) => (
-            <S.Tab key={tab.id} onPress={() => setTabActive(tab.value)}>
-              <S.Text tabs color={tabActive === tab.value ? "#fff" : "#4d4c4c"}>
-                {tab.label}
-              </S.Text>
-            </S.Tab>
-          ))}
-        </S.Tabs>
-      </S.ContainerTabs>
-
-      <S.ContainerInput>
-        <S.SearchIcon name="search" size={20} color="#777" />
-        <S.StyledInput placeholder="Type here..." value={text} onChangeText={setText} />
-      </S.ContainerInput>
-
-      <S.ContainerList>
-        {messages.map((message) => {
-          const lastMessage = message.messages[message.messages.length - 1];
-          const hasUnread = message.unread > 0; 
-
-          return (
-            <TouchableOpacity key={message.id} style={{ width: "100%", marginVertical: 15, height: "auto" }}>
-              <S.Container>
-                <S.ContainerUser>
-                  <S.ContainerAvatar>
-                    <Image
-                      style={{
-                        width: "100%",
-                        height: "100%",
-                        borderRadius: 100,
-                      }}
-                      resizeMode="cover"
-                      source={{ uri: message.photo }}
-                    />
-                    {message.active && (
-                      <S.ContainerBadge>
-                        <S.Badge />
-                      </S.ContainerBadge>
-                    )}
-                  </S.ContainerAvatar>
-
-                  <S.ContainerText>
-                    <S.Title numberOfLines={1}>{message.name}</S.Title>
-                    <S.Text numberOfLines={2} color="#f2f2f2">
-                      {lastMessage}
-                    </S.Text>
-                  </S.ContainerText>
-
-                  {hasUnread && (
-                    <>
-                      <S.Text color="#f2f2f2">+{message.unread}</S.Text>
-                      <S.BadgeBlue />
-                    </>
-                  )}
-                </S.ContainerUser>
-              </S.Container>
-            </TouchableOpacity>
-          );
-        })}
-      </S.ContainerList>
-    </Layout>
+    <>
+      <Messages />
+    </>
   );
-};
+}
